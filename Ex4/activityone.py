52f152e65c1a00eb7dc76a9cf1139740a9ffe830
ashwhi06 <ashe.white06@gmail.com>
2024-02-17 12:47:14 -0500

    added meaningful values and info for Act2

--- Ex4/activityone.py
+++ Ex4/activityone.py
@@ -1,7 +1,7 @@
 # 1. Write a lambda function to sort a list of tuples in ascending order according to the number in the second position of each tuple.
 #Unsorted list of tuples: [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] Sorted list of tuples: [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
-unsorted_list =[('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] 
-sorted_list= sorted(unsorted_list, key=lambda x: x[1])
+unsorted_list =[('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)] #key= 88, 90, 97, 82
+sorted_list= sorted(unsorted_list, key=lambda score: score[1])
 
 print(f"Sorted List:",sorted_list)
 
---
+++ Ex4/activitytwo.py
@@ -0,0 +1,14 @@
+# activity for errors
+fileName = input("Please enter hte name of the file you would like to read: ")
+myfile = open(fileName, 'r') # Open a file for reading.
+contents = myfile.readlines() # Read in the content by line.
+myfile.close() # Explicitly close the file
+print(contents) #print the contents of the file
+
+'''
+test code:
+
+f = FileManipulator("tmp.txt")
+print(f.contents)
+f.reverse("tmp2.txt")
+'''
