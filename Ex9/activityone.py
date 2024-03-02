87b85510361b857ba6e21c44c538d8555756d391
ashwhi06 <ashe.white06@gmail.com>
2024-03-02 12:33:28 -0500

    adding in binary case

--- Ex9/activityone.py
+++ Ex9/activityone.py
@@ -11,3 +11,5 @@ def binary_search(lst, target):
         return binary_search(lst[mid + 1:len(lst)], target)
     elif lst[mid] > target:
         return binary_search(lst[:mid], target)
+    
+print(binary_search([1,2,3,4,5,6,7,8,9,10], 11))
