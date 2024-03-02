faa19bb55b203358e1d2047c58f0f1286b32e3ee
ashwhi06 <ashe.white06@gmail.com>
2024-03-02 12:34:46 -0500

    adding in comments

--- Ex9/activityone.py
+++ Ex9/activityone.py
@@ -1,15 +1,15 @@
 def binary_search(lst, target):
     #base case
-    if len(lst) == 0:
+    if len(lst) == 0: #if the list is empty, return False
         return False
-    mid = len(lst) // 2
-    if lst[mid] == target:
+    mid = len(lst) // 2 #find the middle element of the list
+    if lst[mid] == target: #if the middle element is the target, return True
         return True
     #recursive case
   
-    elif lst[mid] < target:
-        return binary_search(lst[mid + 1:len(lst)], target)
-    elif lst[mid] > target:
-        return binary_search(lst[:mid], target)
+    elif lst[mid] < target: #if the middle element is less than the target, search the right half of the list
+        return binary_search(lst[mid + 1:len(lst)], target) #because the list is sorted, we can assume that the right half of the list is sorted as well
+    elif lst[mid] > target: #if the middle element is greater than the target, search the left half of the list
+        return binary_search(lst[:mid], target) #because the list is sorted, we can assume that the left half of the list is sorted as well
     
 print(binary_search([1,2,3,4,5,6,7,8,9,10], 11))
